561b6d2e67fe8c0f62bbe53639f0be4533653ccc
art69 <@ArtemGuschin>
2024-11-03 17:47:19 +0300

    first commit

--- src/main/java/com/artem/javacore/taskfrommodule1_4/Main.java
+++ src/main/java/com/artem/javacore/taskfrommodule1_4/Main.java
@@ -6,33 +6,27 @@ import java.util.concurrent.CompletableFuture;
 public class Main {
     public static void main(String[] args) {
         Foo foo = new Foo();
-        Thread a = new Thread(() -> {
-            foo.first(() -> {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        });
-        Thread b = new Thread(() -> {
-            foo.second(() -> {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        });
-        Thread c = new Thread(() -> {
-            foo.third(() -> {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        });
+        Thread a = new Thread(() -> foo.first(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+        Thread b = new Thread(() -> foo.second(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+        Thread c = new Thread(() -> foo.third(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }));
         a.start();
         b.start();
         c.start();
