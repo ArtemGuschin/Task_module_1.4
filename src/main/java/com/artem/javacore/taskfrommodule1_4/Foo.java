bd9eb0169fe55fa0f2a44ba20d19934c343a1e9a
art69 <@ArtemGuschin>
2024-11-03 17:30:16 +0300

    first commit

--- src/main/java/com/artem/javacore/taskfrommodule1_4/Foo.java
+++ src/main/java/com/artem/javacore/taskfrommodule1_4/Foo.java
@@ -1,37 +1,51 @@
 package com.artem.javacore.taskfrommodule1_4;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Foo {
-    private boolean firstExecuted;
-    private boolean secondExecuted;
+    ReentrantLock lock = new ReentrantLock();
+    Condition firstMethodCalled = lock.newCondition();
+    Condition secondMethodCalled = lock.newCondition();
 
-    public Foo() {
-        firstExecuted = false;
-        secondExecuted = false;
-    }
-
-    public synchronized void first(Runnable r) {
-        r.run();
-        firstExecuted = true;
-        notifyAll();
-        System.out.print("first");
-    }
-
-    public synchronized void second(Runnable r) throws InterruptedException {
-        while (!firstExecuted) {
-            wait();
+    public void first(Runnable r) {
+        lock.lock();
+        try {
+            r.run();
+            System.out.println("First");
+            firstMethodCalled.signal();
+        } finally {
+            lock.unlock();
         }
-        r.run();
-        secondExecuted = true;
-        notifyAll();
-        System.out.print("second");
+
     }
 
-    public synchronized void third(Runnable r) throws InterruptedException {
-        while (!secondExecuted) {
-            wait();
+    public void second(Runnable r) {
+        lock.lock();
+        try {
+            firstMethodCalled.await();
+            r.run();
+            System.out.println("second");
+            secondMethodCalled.signal();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
         }
-        r.run();
-        System.out.print("third");
+
     }
 
+    public void third(Runnable r) {
+        lock.lock();
+        try {
+            secondMethodCalled.await();
+            r.run();
+            System.out.println("third");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+
+    }
 }
--- src/main/java/com/artem/javacore/taskfrommodule1_4/Main.java
+++ src/main/java/com/artem/javacore/taskfrommodule1_4/Main.java
@@ -4,51 +4,46 @@ import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
-
     public static void main(String[] args) {
         Foo foo = new Foo();
-
-
-        Thread a = new Thread(() -> foo.first(() -> {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }));
-
-        Thread b = new Thread(() -> {
-            try {
-                foo.second(() -> {
+        Thread a = new Thread(() -> {
+            foo.first(new Runnable() {
+                @Override
+                public void run() {
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
-                });
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+                }
+            });
         });
-
-
-        Thread c = new Thread(() -> {
-            try {
-                foo.third(() -> {
+        Thread b = new Thread(() -> {
+            foo.second(new Runnable() {
+                @Override
+                public void run() {
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
-                });
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+                }
+            });
+        });
+        Thread c = new Thread(() -> {
+            foo.third(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
         });
         a.start();
         b.start();
         c.start();
-
-
     }
 }
